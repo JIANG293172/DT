d7c6c6a7cad0363cb020e032b4e5e6b371ece39d
JIANG293172 <904885694@qq.com>
2018-01-19 19:33:39 +0800

    DTBase

---
+++ DT/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DT
+//
+//  Created by tao on 18/1/19.
+//  Copyright © 2018年 tao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DT/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  DT
+//
+//  Created by tao on 18/1/19.
+//  Copyright © 2018年 tao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
